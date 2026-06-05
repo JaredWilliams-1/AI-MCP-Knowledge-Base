740acc7a9602125d3a4a6ef0866d37de63151cb6
Jared Williams <jaredlukewilliams@gmail.com>
2026-06-05 20:13:55 +0200

    Set up the basic IO

--- MCP Server/server.go
+++ MCP Server/server.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+    "bufio";
+    "os";
+    "fmt"
+)
 
 func main() {
+    scanner := bufio.NewScanner(os.Stdin)
+    for scanner.Scan() {
+        line := scanner.Text()
 
+        fmt.Println(line)
+    }
 }
